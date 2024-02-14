7313446908f31e61ccc4585be312f565a4b5efea
jackhsuwork345 <160015059+jackhsuwork345@users.noreply.github.com>
2024-02-14 18:37:16 +0800

    Apply suggestions from code review
    Co-authored-by: Mayavi <51351718+Mayavi345@users.noreply.github.com>

--- 24.Counter2.js
+++ 24.Counter2.js
@@ -12,7 +12,6 @@ function Intersection(arr1, arr2) {
     if (!counter[array3Value]) {
       counter[array3Value] = 1;
     } else {
-      //這邊的註解做一下更改
       //這邊的註解已經做了調整
       counter[array3Value]++;
     }
@@ -20,7 +19,12 @@ function Intersection(arr1, arr2) {
   console.log(counter);
   //我多了幾個console
   console.log(counter);
+    console.log(counter);
   console.log(counter);
+  console.log(counter);
+  console.log(counter);
+  console.log(counter);
+
 
   //Loop over the counter
   for (let property in counter) {
